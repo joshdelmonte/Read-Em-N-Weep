bb137d57ba5649f9ccea4f06cf25fe1a0585e960
joshdelmonte <joshdelmonte@gmail.com>
2022-10-10 00:04:52 -0600

    First question set up

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
-const fs = require('fs')
+const fs = require('fs');
+const { type } = require('os');
+const { title } = require('process');
 // TODO: Create an array of questions for user input
-const pergunta = [];
+const pergunta = [
+    {
+        message: ' What name would you like to give your README?',
+        type: 'input',
+        name:'titulo'
+    }
+];
+
+inquirer.createPromptModule(pergunta)
+.then(riposta => {
+    console.log(riposta)
+})
+.catch(err=> console.log(err))
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
