314a38493e6b0e856953a2d2576957a75157a97e
joshdelmonte <joshdelmonte@gmail.com>
2022-10-18 17:22:21 -0600

    Corrected License problem hopefully

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,18 +8,18 @@
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === `APACHE 2.0`) {
-    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+function renderLicenseSection(licensa) {
+  if (licensa === `APACHE 2.0`) {
+    return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)"
   }
-  else if (license === `Hippocratic License 2.1`) {
-    return "[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)"
+  else if (licensa === `Hippocratic License 2.1`) {
+    return "![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)"
   }
-  else if (license === `Mozilla Public License 2.0`) {
-    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  else if (licensa === `Mozilla Public License 2.0`) {
+    return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)"
   }
-  else if (license === `MIT License`){
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  else if (licensa === `MIT License`){
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
   }
    
 }
