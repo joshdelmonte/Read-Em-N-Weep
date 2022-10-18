94b2cb46de61e0018fb4c484849cb59f8daa16d5
joshdelmonte <joshdelmonte@gmail.com>
2022-10-18 16:51:27 -0600

    Added HTML to the js code but am questioning if I need it

--- index.js
+++ index.js
@@ -5,6 +5,53 @@ const { type } = require('os');
 const { title } = require('process');
 const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
+const generateHTML = ({licensa, titulo, descricao, instalacao, contribucao, testing, username, email }) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1>Title: ${titulo}</h1>
+        <!-- Insert tags for the licences here -->
+        <img src="" alt="">
+        <h2>Description</h2>
+        <p>${descricao}</p>
+        <ul>
+            <li><a href="#descricao">Description</a></li>
+            <li><a href="#instalacao">Installation</a></li>
+            <li><a href="#contribucao">Contributions</a></li>
+            <li><a href="#testing">Testing</a></li>
+            <li><a href="#contact"></a>Contact</li>
+        </ul>
+
+        <h2 id="instalacao">Installation</h2>
+        <p>${instalacao}</p>
+
+        <h2 id="contribucao">Contributions</h2>
+        <p>${contribucao}</p>
+
+        <h2 id="testing">Testing</h2>
+        <p>${testing}</p>
+
+        <h2 id="contact">Contact</h2>
+        <p>${username}</p>
+        <p>${email}<p>
+
+
+
+
+    </div>
+</div>
+<body>
+    
+</body>
+</html>`
+
 const pergunta = [
     {
         type: 'input',
@@ -45,7 +92,7 @@ const pergunta = [
     {
         type: 'input',
         message: ' Would you please enter your e-mail username for contact purposes?',
-        name:'e-mail'
+        name:'email'
     },
 ];
 
@@ -55,6 +102,7 @@ inquirer.prompt(pergunta)
     writeToFile(`./output/README.md`, riposta) 
 })
 .catch(err=> console.log(err))
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, riposta){
